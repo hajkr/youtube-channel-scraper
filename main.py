bed92be0fe29c85f3a83528b3e423c7a7c353a62
Tadej <tadej.996@gmail.com>
2021-05-15 13:21:23 +0200

    Change csv delimiters.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from crawler import Crawler
 def write_to_csv(items, filename):
     keys = items[0].keys()
     with open(filename, 'w', encoding=locale.getpreferredencoding()) as output_file:
-        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer = csv.DictWriter(output_file, keys, delimiter=";", quoting=csv.QUOTE_NONNUMERIC)
         dict_writer.writeheader()
         dict_writer.writerows(items)
 
