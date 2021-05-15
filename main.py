2d95fe99957375a4f0e6cbae9cab9f1ac71d37d5
Tadej <tadej.996@gmail.com>
2021-05-15 12:46:04 +0200

    Write crawled videos to csv.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+import csv
+import locale
 from crawler import Crawler
+
+
+def write_to_csv(items, filename):
+    keys = items[0].keys()
+    with open(filename, 'w', encoding=locale.getpreferredencoding()) as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(items)
 
 
 channel_url = "https://www.youtube.com/user/anzecokl/videos"
 
 crawler = Crawler("https://www.youtube.com/user/anzecokl")
-crawler.crawl()
+crawled_videos = crawler.crawl()
+
+print("Writing to csv...")
+write_to_csv(crawled_videos, "videos.csv")
